d370c08b17a01485d416194145b2b5f94a64e8c9
UtkuKaganYavuzdogannn <Utku_K_Y@outlook.com>
2025-04-02 18:55:02 +0300

    css değişiklikleri

--- src/CryptoTable.js
+++ src/CryptoTable.js
@@ -10,40 +10,36 @@ const CryptoTable = () => {
     fetch("https://localhost:7215/api/investment/prices") // .NET Core API'yi çağır
       .then((response) => response.json()) // JSON formatına çevir
       .then((data) => {
-        // JSON'u uygun hale getir
         const transformedData = Object.entries(data).map(([name, details]) => ({
           name,
           price: details.usd, // "usd" değerini al
         }));
         setCoins(transformedData); // State'e kaydet
-      })
-      .catch((error) => console.error("Veri çekme hatası:", error));
+      });
   }, []); // Boş array, sadece 1 kez çalışmasını sağlar
 
   return (
     <div>
       <h2>Cryptos and gold-silver etc.</h2>
-      <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Ad</th>
-              <th>Fiyat (USD)</th>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price (USD)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.name}>
+              <td>{coin.name}</td>
+              <td>${coin.price}</td>
             </tr>
-          </thead>
-          <tbody>
-            {coins.map((coin) => (
-              <tr key={coin.name}>
-                <td>{coin.name}</td>
-                <td>${coin.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-  
 };
+
 
 export default CryptoTable;
